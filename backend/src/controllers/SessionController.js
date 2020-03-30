1121fba2f2bba3be90b7d8464ad601587272b9b5
matheusribeirog <matheus.gomes94@etec.sp.gov.br>
2020-03-29 21:10:58 -0300

    uploading the source code

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,12 @@
+const connection=require('../database/connection')
+module.exports={
+    async create(request,response){
+        const {id}=request.body;
+        const ong=await connection('ongs').where('id',id).select('name').first();
+
+        if(!ong){
+            return response.status(400).json({error:"deu ruim"})
+        }
+        return response.json(ong)
+    }
+}
